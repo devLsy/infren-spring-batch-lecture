0ee895a842f98f3b2c01463fc96a8cddfa1e0221
devlsy86 <infightersy@naver.com>
2023-11-13 19:44:16 +0900

    first commit

---
+++ src/main/java/com/dev/lsy/infrenspringbatchstudy/InfrenSpringBatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.dev.lsy.infrenspringbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrenSpringBatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrenSpringBatchStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/lsy/infrenspringbatchstudy/InfrenSpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.lsy.infrenspringbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrenSpringBatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
