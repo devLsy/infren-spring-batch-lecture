9850c1d2a240cf545d6152e23c34720b35c0cede
devLsy(office) <infightersy@naver.com>
2023-11-23 15:22:00 +0900

    구조를 변경했음

---
+++ batch-service/src/main/java/com/dev/lsy/batchservice/BatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dev.lsy.batchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchServiceApplication.class, args);
+    }
+
+}
---
+++ batch-service/src/main/java/com/dev/lsy/batchservice/batch/config/JobConfig.java
@@ -0,0 +1,5 @@
+package com.dev.lsy.batchservice.batch.config;
+
+
+public class JobConfig {
+}
